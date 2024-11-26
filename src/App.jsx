05630934caf3a6fc071c5b1c0cbfb96c34aadb44
Alika <dangolalika2017@gmail.com>
2024-11-26 22:12:56 +0545

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Outlet } from 'react-router-dom'
 import Home from './pages/Home'
 import { Swiper } from 'swiper/react'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <>
-    
+    <Header/>
     <Outlet/>
     {/* <Swiper/> */}
     <Footer/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
-    header
+<section className='py-5 bg-[#2F2D2D]' >
+  <div className='px-11 lg:px-44 flex justify-between bg-[#2F2D2D]'>
+    <div><h2 className='text-[#F50000] text-3xl'>DiveInCinema</h2></div>
+    <nav className='flex gap-3 text-white'>
+      <li><Link>Comedy</Link></li>
+      <li><Link>Comedy</Link></li>
+      <li><Link>Comedy</Link></li>
+      <li><Link>Comedy</Link></li>
+      <li><Link>Comedy</Link></li>
+      <li><Link>Comedy</Link></li>
+      
+    </nav>
+
+  </div>
+</section>
       
     </>
   )
