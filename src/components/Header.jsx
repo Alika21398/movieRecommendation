c430d370b212f0c79153f73ae189debe13c12d7a
Alika <dangolalika2017@gmail.com>
2024-11-27 22:10:39 +0545

    modified header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-<section className='py-5 bg-[#2F2D2D]' >
-  <div className='px-11 lg:px-44 flex justify-between bg-[#2F2D2D]'>
-    <div><h2 className='text-[#F50000] text-3xl'>DiveInCinema</h2></div>
-    <nav className='flex gap-3 text-white'>
-      <li><Link>Comedy</Link></li>
-      <li><Link>Comedy</Link></li>
-      <li><Link>Comedy</Link></li>
-      <li><Link>Comedy</Link></li>
-      <li><Link>Comedy</Link></li>
-      <li><Link>Comedy</Link></li>
-      
-    </nav>
-
-  </div>
-</section>
-      
+      <section className="py-5 bg-[#2F2D2D]">
+        <div className="px-11 lg:px-44 flex justify-between bg-[#2F2D2D]">
+          <div className="flex gap-5 items-center">
+            <div>
+              <h2 className="text-[#F50000] text-3xl">DiveInCinema</h2>
+            </div>
+            <form action="">
+              <input className="bg-[#695D5D] px-6 py-3 rounded-xl flex flex-grow" type="text" placeholder="search..." />
+            </form>
+          </div>
+          <nav className="flex gap-3 text-white">
+            <li>
+              <Link>Comedy</Link>
+            </li>
+            <li>
+              <Link>Comedy</Link>
+            </li>
+            <li>
+              <Link>Comedy</Link>
+            </li>
+            <li>
+              <Link>Comedy</Link>
+            </li>
+            <li>
+              <Link>Comedy</Link>
+            </li>
+            <li>
+              <Link>Comedy</Link>
+            </li>
+          </nav>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
