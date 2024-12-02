78f2fc89b69cb1a95ae8e0f2eaa31370bfc038dd
Alika <dangolalika2017@gmail.com>
2024-12-02 08:02:18 +0545

    edited footer and added new image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React from "react";
+
+import footerImg from '../assets/footer-img.png'
 
 const Footer = () => {
   return (
     <>
-    footer Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quas minima reprehenderit iure modi quae a facilis odit, voluptates at facere recusandae, optio maxime cum quisquam. Veritatis, eveniet inventore. Neque aspernatur provident error, iure eius incidunt cumque laborum? Nam accusamus laborum asperiores perspiciatis architecto enim obcaecati recusandae corrupti deleniti, nostrum delectus non similique inventore doloremque molestias provident labore placeat. Magnam blanditiis assumenda iste deserunt, dolorum totam omnis animi adipisci quos dolor ducimus, sint nisi? Corporis totam vel dicta nulla cumque nemo accusamus. At velit vel, quia cum perspiciatis quam. Cupiditate repellat itaque autem assumenda sequi, harum ipsam aut! Consequuntur, officiis!
-      
-    </>
-  )
-}
+      <footer className="bg-[#F50000]">
+        <div className="px-11 lg:px-44 py-10 lg:py-20 text-white">
+          <div className="flex justify-between items-start">
+            <div>
+              <div className="pb-5">
+                <h2 className="text-[#F50000] px-4 py-2 bg-white text-2xl lg:text-4xl">DiveInCinema</h2>
+              </div >
+              <div>
+                <h3>Patan Multiple Campus</h3>
+                <h3 className="py-3">Patan Dhoka</h3>
+                <a className="hover:text-[#ffffff81] underline" href="mailto:diveIncinema@gmail.com">diveIncinema@gmail.com</a>
+              </div>
+            </div>
+            <div>
+              <h3 className="font-bold text-2xl py-3">Company</h3>
+<ul className="leading-10">
+  <li>About</li>
+<li>Our Culture</li>
+<li>Giving</li>
+<li>Partners</li>
+<li>Careers</li>
+<li>News</li>
 
-export default Footer
+
+<li>Advertise with Us</li>
+</ul>
+
+            </div>
+            <div>
+              <img src={footerImg} alt="" />
+           
+            <div className="flex gap-5 py-10">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-twitter"></i>
+            </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="py-8 border-t border-[#ffff] text-center text-[#ffffff]">Copyright © 2024 DiveInCinema 
+        </div>
+
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
