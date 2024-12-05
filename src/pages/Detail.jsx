afd09093673035f8e860c1d13ca3eb1fff7b0968
Alika <dangolalika2017@gmail.com>
2024-12-05 12:41:18 +0545

    added detail page to show detail of each movie

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main_Header from '../components/Main_Header'
+
+const Detail = () => {
+  return (
+    <>
+    <Main_Header/>
+      
+    </>
+  )
+}
+
+export default Detail
