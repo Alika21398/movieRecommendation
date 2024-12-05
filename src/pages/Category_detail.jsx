a2621e310751a0baf9bda373d8d49b424a9dcc9d
Alika <dangolalika2017@gmail.com>
2024-12-05 15:48:41 +0545

    removed border and added transition in image

--- src/pages/Category_detail.jsx
+++ src/pages/Category_detail.jsx
@@ -8,8 +8,8 @@ const Category_detail = () => {
     <>
       <section className="px-11 lg:px-44">
         {/* Filtering content starts */}
-        <div className="">
-          <label htmlFor="">Choose the genre</label>
+        <div className="my-5">
+          <label htmlFor="" className="mr-2">Choose the genre</label>
           <select className="px-4 py-2 border border-[#F50000]" name="" id="">
             <option value="">Comedy</option>
             <option value="">Romantic</option>
@@ -20,81 +20,124 @@ const Category_detail = () => {
         </div>
         {/* Filtering content ends */}
 
-        <div className="flex flex-wrap my-10 gap-5">
+        <div className="flex flex-wrap my-10 justify-between">
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="py-2 px-4  mt-2">
+              <div className="w-[100px] lg:w-[200px]  overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md " src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
-              <div className=" py-2">
+              <div className=" pt-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+                
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+            <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md " src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
+              </div>
+              <div className=" pt-2">
+              <h3>Justice League</h3>
+           
+                </div>
+            </div>
+          </Link>
+          <Link to="/details/comedy">
+          <div className="pt-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
               <div className=" py-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+                
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="pt-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
               <div className=" py-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+               
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md " src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
-              <div className=" py-2">
+              <div className=" pt-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+                
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md " src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
-              <div className=" py-2">
+              <div className=" pt-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+               
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
-              <div className=" py-2">
+              <div className=" pt-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+                
               </div>
             </div>
           </Link>
           <Link to="/details/comedy">
-            <div className="p-4 border">
-              <div>
-                <img src={photo1} alt="" />
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
               </div>
-              <div className=" py-2">
+              <div className=" pt-2">
                 <h3>Justice League</h3>
-                <p className="text-[#F50000]">Imdb: 7.5</p>
+               
+              </div>
+            </div>
+          </Link>
+          <Link to="/details/comedy">
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
+              </div>
+              <div className=" pt-2">
+                <h3>Justice League</h3>
+              
+              </div>
+            </div>
+          </Link>
+          <Link to="/details/comedy">
+          <div className="py-2 px-4   mt-2">
+              <div className="w-[100px] lg:w-[200px] overflow-hidden relative">
+                <img className="hover:translate-y-1 hover:transform hover:scale-110 transition duration-500 ease-in-out rounded-md" src={photo1} alt="" />
+                <p className="bg-[#F50000] absolute top-0 right-0 px-2 py-1 text-white">Imdb: 7.5</p>
+              </div>
+              <div className=" pt-2">
+                <h3>Justice League</h3>
+                
               </div>
             </div>
           </Link>
