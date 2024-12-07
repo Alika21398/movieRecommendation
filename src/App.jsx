fed58f12bb7ad0fd8cee47ef3e7f2590c8454cdb
Alika <dangolalika2017@gmail.com>
2024-12-07 21:40:55 +0545

    added context adpi

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Home from './pages/Home'
 import { Swiper } from 'swiper/react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
+export const Apidata = createContext()
+
+export function useApi(){
+  return useContext(Apidata)
+}
+
 const App = () => {
+  const[show, setShow] = useState(false)
+
+  console.log("show", show)
   return (
-    <>
+    <Apidata.Provider value={{show, setShow}}>
     {/* <Header/> */}
     <Outlet/>
     {/* <Swiper/> */}
     <Footer/>
       
-    </>
+    </Apidata.Provider>
   )
 }
 
